e9db7b79d7b78d3831415b8fa337f943ecaafcf4
Mateus Lucas <mateuslucas1879@gmail.com>
2024-06-14 01:44:34 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <title>Cadastrar</title>
+  </head>
+  <body>
+    <header></header>
+
+    <main class="container">
+        <h1>Cadastrar</h1>
+
+        <form>
+                <div class="form-group">
+                    <label for="nome">Nome completo</label>
+                    <input type="twxt" class="form-control" id="nome">
+                    
+                </div>
+                <div class="form-group">
+                    <label for="endereco">Endereço </label>
+                    <input type="text" class="form-control" id="endereco">
+                    
+                </div>
+                <div class="form-group">
+                    <label for="telefone">Telefone</label>
+                    <input type="text" class="form-control" id="telefone">
+                    
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" id="email1">
+                    
+                </div>
+                <div class="form-group">
+                    <label for="data">Data de Nascimento</label>
+                    <input type="date" class="form-control" id="date">
+                    
+                </div>
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </form>
+        </main>
+
+    <footer></footer>
+
+    
+
+
+  </body>
+</html>
